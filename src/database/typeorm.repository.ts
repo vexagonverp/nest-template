dd7c8f200c75de5a9d4430972a12034298a7a18f
Bao Thanh <verproti80@yahoo.com.vn>
2024-04-09 15:16:26 +0700

    typeOrm

--- src/database/typeorm.repository.ts
+++ src/database/typeorm.repository.ts
@@ -1,9 +1,13 @@
 import {
+  DeepPartial,
   FindManyOptions,
   FindOneOptions,
   FindOptionsWhere,
+  InsertResult,
   Repository,
+  UpdateResult,
 } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
 export class TypeOrmRepository<Entity> {
   constructor(public repository: Repository<Entity>) {}
@@ -41,5 +45,28 @@ export class TypeOrmRepository<Entity> {
     });
 
     return results;
+  }
+
+  async update(
+    where: FindOptionsWhere<Entity>,
+    update: QueryDeepPartialEntity<Entity>,
+  ): Promise<UpdateResult> {
+    const result = await this.repository.update(where, update as any);
+
+    return result;
+  }
+
+  async save(entity: DeepPartial<Entity>): Promise<DeepPartial<Entity>[]> {
+    const result = await this.repository.save([entity]);
+
+    return result;
+  }
+
+  async insert(
+    entity: QueryDeepPartialEntity<Entity> | QueryDeepPartialEntity<Entity>[],
+  ): Promise<InsertResult> {
+    const result = await this.repository.insert(entity);
+
+    return result;
   }
 }
